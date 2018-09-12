3960c87af2e256d21a18fd7fe5007ebebb234d3d
Juan Pedro Dominguez-Morales <jpdominguez@atc.us.es>
2018-09-12 20:51:50 +0200

    Updated folders

--- OpenNAS/OpenNAS/Properties/AssemblyInfo.cs
+++ OpenNAS/OpenNAS/Properties/AssemblyInfo.cs
@@ -7,12 +7,12 @@ using System.Windows;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("OpenNAS_App")]
+[assembly: AssemblyTitle("OpenNAS")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("OpenNAS_App")]
-[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyProduct("OpenNAS")]
+[assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
